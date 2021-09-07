ac02f1006d230ddd004e69e19dbc2557a3f2a8cd
Sergio Ribeiro <ssribeiro@gmail.com>
2021-09-07 12:47:06 -0300

    casesPage_gridCall

--- react_app/src/cases/Cases.jsx
+++ react_app/src/cases/Cases.jsx
@@ -5,16 +5,22 @@ import { Link } from "react-router-dom";
 import './Cases.css';
 
 // All Components import
-// import { Case, CasesGridView } from '../components/case';
+import { Case, CasesGridView } from '../components/case';
 
 function Cases() {
+
+    const caseData = [  {id:1, name:"Case 1",description:"Demo Case 1",  created:"01/01/2021 09:00:00", modified:"01/03/2021 09:00:00", lastrun:"01/03/2021 09:00:00"},
+                        {id:2, name:"Case 2",description:"Demo Case 2",  created:"01/04/2021 09:00:00", modified:"05/03/2021 09:00:00", lastrun:"05/04/2021 09:00:00"}
+                    ];
 
     // Variables and Constants
 
     return (
         // React Browser Router
         <div className="scope" style={{color:"white",textDecoration:"none"}}>
-            <Link to="/cases_1"> Cases </Link>
+            <h4> User's Cases: </h4>
+            <CasesGridView caseData={caseData}/> 
+            {/* </CasesGridView>data={casesData}/> */}
         </div>
     )
 };
