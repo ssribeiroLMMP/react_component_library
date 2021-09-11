a1dca0744d6b9b88791cd42123f33217d484509b
Sergio Ribeiro <ssribeiro@gmail.com>
2021-09-11 11:57:27 -0300

    separateDataObjects

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import { ThemeProvider } from 'styled-components';
 // import { theme } from './theme'
 import './App.css';
+import {navlinks, formData, accordionData, conversionFactors} from "./data"
 
 // All Components import
 import { Header, GridForm, 
@@ -15,26 +16,7 @@ import { Cases } from "./cases";
 
 function App() {
 
-  // Set navlinks Object
-  const navlinks = [
-    {url: "/" , title: "Home", className: "navbar-item"},
-    {url: "/cases" , title: "Cases", className: "navbar-item"},
-    {url: "/control" , title: "Control", className: "navbar-item"},
-    {url: "/help" , title: "Help", className: "navbar-item"}]
-    
-  //TODO: Bring all dynamic data from the Flask API
-  const conversionFactors = { lenght: {"km": 1000, "cm": 1/100, "mm": 1/1000, "um": 1/1000000, "m": 1},
-                              pressure: {"psi": 0.000145038, "mmHg": 0.00750062, "Pa": 1,}};
-  
-  const  formData = {gap: {label: "Gap:", defaultValue:"0", unitType:"lenght", selectecUnit:"mm"},
-                size: {label: "Size:", defaultValue:"0", unitType:"lenght", selectecUnit:"cm"},
-                p0: {label: "Inlet Pressure:", defaultValue:"0", unitType:"pressure", selectecUnit:"psi"}};
-  
-  const actionData = {actionButtom: {tootip:"Grupo de Ações" ,icon:"fas fa-plus" ,rotate:true, 
-                                      backgroundColor: darkColors.blue ,color: lightColors.white},
-                      actions: [{tooltip:"Ação 0" ,icon:"fas fa-react" ,key:"action1" ,url:"/" ,backgroundColor: darkColors.blue ,color: lightColors.white},
-                                {tooltip:"Ação 1" ,icon:"fas fa-minus" ,key:"action2" ,url:"/" ,backgroundColor: darkColors.blue ,color: lightColors.white}
-                                ]};
+
   return (
     // React Browser Router
     <Router>
---
+++ react_app/src/data.js
@@ -0,0 +1,38 @@
+export {accordionData, navlinks, conversionFactors, formData, actionData}
+
+const accordionData = [
+    {
+      question: 'What do you call a dog magician?',
+      answer: 'A labracadabrador.'
+    },
+    {
+      question: 'What do you call a funny mountain?',
+      answer: 'Hill-arious.'
+    },
+    {
+      question: 'What did the astronaut say when he crashed into the moon?',
+      answer: 'I Apollo-gize.'
+    }
+  ];
+
+
+  // Set navlinks Object
+  const navlinks = [
+    {url: "/" , title: "Home", className: "navbar-item"},
+    {url: "/cases" , title: "Cases", className: "navbar-item"},
+    {url: "/control" , title: "Control", className: "navbar-item"},
+    {url: "/help" , title: "Help", className: "navbar-item"}]
+    
+  //TODO: Bring all dynamic data from the Flask API
+  const conversionFactors = { lenght: {"km": 1000, "cm": 1/100, "mm": 1/1000, "um": 1/1000000, "m": 1},
+                              pressure: {"psi": 0.000145038, "mmHg": 0.00750062, "Pa": 1,}};
+  
+  const  formData = {gap: {label: "Gap:", defaultValue:"0", unitType:"lenght", selectecUnit:"mm"},
+                size: {label: "Size:", defaultValue:"0", unitType:"lenght", selectecUnit:"cm"},
+                p0: {label: "Inlet Pressure:", defaultValue:"0", unitType:"pressure", selectecUnit:"psi"}};
+  
+  const actionData = {actionButtom: {tootip:"Grupo de Ações" ,icon:"fas fa-plus" ,rotate:true, 
+                                      backgroundColor: darkColors.blue ,color: lightColors.white},
+                      actions: [{tooltip:"Ação 0" ,icon:"fas fa-react" ,key:"action1" ,url:"/" ,backgroundColor: darkColors.blue ,color: lightColors.white},
+                                {tooltip:"Ação 1" ,icon:"fas fa-minus" ,key:"action2" ,url:"/" ,backgroundColor: darkColors.blue ,color: lightColors.white}
+                                ]};
