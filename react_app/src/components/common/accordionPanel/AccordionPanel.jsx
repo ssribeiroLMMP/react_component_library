b4f4f2e74a0b34fdd6a9fd8e0a685bd2ddc8332a
Sergio Ribeiro <ssribeiro@gmail.com>
2021-09-13 06:08:46 -0300

    jsxComponentnoStyle

--- react_app/src/components/common/accordionPanel/AccordionPanel.jsx
+++ react_app/src/components/common/accordionPanel/AccordionPanel.jsx
@@ -2,24 +2,63 @@
 import React, { useState } from 'react';
 
 // # Import Component Style
-// import './AccordionPanel.css'
+import './AccordionPanel.css';
+
+function AccordionSection(props) {
+    return (
+        <div className={"accordion_section"}>
+            {props.children}
+        </div>
+    )
+}
+
+function AccordionContainer(props) {
+    return (
+        <div className={"accordion_container"}>
+            {props.children}
+        </div>
+    )
+}
+
+function AccordionWrap(props) {
+    return (
+        <div className={"accordion_wrap"}>
+            {props.children}
+        </div>
+    )
+}
+
+function AccordionDropdown(props) {
+    return (
+        <div className={"accordion_dropdown"}>
+            {props.children}
+        </div>
+    )
+}
 
 // # Logo React Component Construction
 function Accordion (props) {
     
     const accordionData = props.data;
     return (
-        <div>
-            {accordionData.map((item, index) => {
-                return (
-                    <div key={["accordion_"+index]}>
-                        <h1>{item.main}</h1>
-                        <p>{item.hidden}</p>
-                    </div>
-                    )
-            })}
-            
-        </div>
+        <AccordionSection>
+            {/* <AccordionContainer>
+                <div>
+                    {accordionData.map((item, index) => {
+                        return (
+                            <>
+                            <AccordionWrap key={["accordion_"+index]}>
+                                <h1>{item.main}</h1>
+                            </AccordionWrap>
+                            <AccordionDropdown>
+                                <p>{item.hidden}</p>
+                            </AccordionDropdown>
+                            </>
+                            )
+                    })}
+                </div>
+            </AccordionContainer> */}
+        </AccordionSection>
     )
 }
 
